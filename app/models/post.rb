a6169fcf83f84abae7751df7811015945d7860c5
Mucahid Canbey <mucahid.canbey@gmail.com>
2023-01-25 04:11:36 +0300

    Creating The Blog Post CRUD Scaffold

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    validates :title, presence: :true, length: {minimum:5, maximum:40}
+    validates :body, presence: :true, length: {minimum:10, maximum:700}
+
+end
+
