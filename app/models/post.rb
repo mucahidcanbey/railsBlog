b6df06c0bf03a610ad17856e84a407639e771db2
Mucahid Canbey <mucahid.canbey@gmail.com>
2023-02-05 01:19:35 +0300

    How To Do Associations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,7 @@
 class Post < ApplicationRecord
-    validates :title, presence: :true, length: {minimum:5, maximum:40}
-    validates :body, presence: :true, length: {minimum:10, maximum:700}
+    validates :title, presence: true, length: {minimum:5, maximum:40}
+    validates :body, presence: true, length: {minimum:10, maximum:700}
+    belongs_to :user 
 
 end
 
-10.times do |x| 
-    Post.create(title:"Title #{x}", body: "Body #{x} words go here Idk")
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+user = User.new(email: 'test@gmail.com', password: 'password', password_confirmation: 'password')
+user.save!
+
+10.times do |x|
+  post = Post.new(title: "Title #{x}", body: "Body #{x} Words go here Idk", user: user)
+  post.save!
+end
